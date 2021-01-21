db40feeac61b31d200f729e014effc44f397c120
Wilhelm Berggren <wilhelmberggren@hotmail.com>
2021-01-21 10:04:24 +0100

    Added tests for top list ascending/descending

--- MovieLibraryTests/MovieLibraryTests.cs
+++ MovieLibraryTests/MovieLibraryTests.cs
@@ -18,11 +18,46 @@ namespace MovieLibraryTests
             };
             var repository = new MockedMovieRepository(movies);
             var controller = new MovieController(repository);
+
             Assert.AreEqual(movies, repository.GetMovies());
 
             var expected = movies.Select(m => m.title).First();
             var actual = controller.Toplist(true).First();
             Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void TestTopListAscending()
+        {
+            var movies = new[] {
+                new Movie { id = "1", title = "a", rated = "5,0"},
+                new Movie { id = "2", title = "b", rated = "6,0"}
+            };
+            var repository = new MockedMovieRepository(movies);
+            var controller = new MovieController(repository);
+
+            var expected = controller.Toplist(true);
+            var actual = movies.OrderBy(m => m.rated).Select(m => m.title);
+
+            Assert.AreEqual(expected.First(), actual.First());
+            Assert.AreEqual(actual.First(), "a");
+        }
+        
+        [TestMethod]
+        public void TestTopListDescending()
+        {
+            var movies = new[] {
+                new Movie { id = "1", title = "a", rated = "5,0"},
+                new Movie { id = "2", title = "b", rated = "6,0"}
+            };
+            var repository = new MockedMovieRepository(movies);
+            var controller = new MovieController(repository);
+
+            var expected = controller.Toplist(false);
+            var actual = movies.OrderByDescending(m => m.rated).Select(m => m.title);
+
+            Assert.AreEqual(expected.First(), actual.First());
+            Assert.AreEqual(actual.First(), "b");
         }
     }
 }
